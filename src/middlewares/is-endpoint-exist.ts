2f6e30d474d7dda9b00b02c518bc706cceb90156
Can Tuncer <mcantuncerr@gmail.com>
2022-06-12 15:19:19 +0100

    is-endpoint-exist middleware update

--- src/middlewares/is-endpoint-exist.ts
+++ src/middlewares/is-endpoint-exist.ts
@@ -1,12 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import { getEndpointDetails } from '../helpers/endpoint';
 import { RESPONSE_STATUS } from '../constants/enums';
+import { IRequestData } from '../constants/interfaces';
 
 export const isEndpointExistMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  req.reqData = {} as IRequestData;
   const epDetails = getEndpointDetails(req);
 
   if (!epDetails)
